05a35a6a3ea939f244644f6c775dfe99dbe35c2b
Pablo Gonzalez <pablo@sementedigital.com.br>
2020-06-18 02:16:40 -0300

    [18/06/2020][ :sparkles: ] Adicionando rota 'update' para item e item_group

--- routes/items.js
+++ routes/items.js
@@ -92,6 +92,40 @@ router.post('/create_item', async function(req, res, next) {
 })
 
 /**
+* Update an item.
+**/
+router.post('/update_item/:id', async function(req, res, next) {
+    let item = {
+        title: req.body.title || null,
+        description: req.body.description || null,
+        image: req.body.image || null,
+        group_id: req.body.group_id || null
+    }
+
+    if(item.title && item.description) {
+        try {
+            await getConnection().query(
+                `UPDATE items 
+                SET title='${item.title}', description='${item.description}', image='${item.image}', group_id='${item.group_id}' 
+                WHERE id=${req.params.id}`, 
+                (error, results) => {
+                if(error) {
+                    throw error;
+                }
+                console.log('requisição completada com sucesso');
+                res.status(200).json({msg:'requisição completada com sucesso'});
+            })
+        } catch (error) {
+            console.error('erro ao fazer requisição: ', error);
+            res.status(500).json({msg: 'erro ao fazer a requisição', obj:error.message});
+        }
+    } else {
+        console.error('erro ao fazer requisição: ');
+        res.status(500).json({msg: 'Cheque se os campos foram preenchidos adequadamente', obj:item});
+    }
+})
+
+/**
 * Delete an item.
 **/
 router.delete('/delete_item/:id', async function(req, res, next) {
