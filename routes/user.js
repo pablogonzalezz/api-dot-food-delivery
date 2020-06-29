2d345b61866d825080e572a7828354bc3445a2ac
Pablo Gonzalez <pablo@sementedigital.com.br>
2020-06-29 03:01:48 -0300

    [29/06/2020][ :sparkles: ] Adicionando rota de pegar telefone do usuario

--- routes/user.js
+++ routes/user.js
@@ -27,4 +27,29 @@ router.get('/get_user_address/:login', async function(req, res, next) {
     }
 })
 
+router.get('/get_user_phone/:login', async function(req, res, next) {
+    
+    if (req.params.login) {
+        try {
+            await getConnection().connect( async (err, client, done) => {
+                await client.query(
+                    `SELECT phone FROM users WHERE login='${req.params.login}'`
+                ).then(results => {
+                    if(err) {
+                        console.error('error: ', err);
+                    }
+                    done();  
+                    res.status(200).json(results.rows[0].phone);
+                })
+            });
+        } catch (error) {
+            console.error('erro ao fazer requisição: ', error);
+            res.status(500).json({msg: 'erro ao fazer a requisição', obj:error.message});
+        }
+    } else {
+        console.error('erro ao fazer requisição: ');
+        res.status(500).json({msg: 'Cheque se os campos foram preenchidos adequadamente'});
+    }
+})
+
 module.exports = router;
